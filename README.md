e0fec43cd37118040811998f0d1a8dd7c2f0bd1f
samutamm <samutamm@gmail.com>
2018-04-07 16:30:03 +0200

    add node server

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# facebook-charllenge" 
+
+To start server in localhost:3000
+``` 
+npm install
+npm start  
+ ```
+ 
---
+++ server.js
@@ -0,0 +1,10 @@
+
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+
+app.listen(3000);
